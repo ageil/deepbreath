49075b853d6d45da61103b0bfb04641c828252b0
Anders Geil <anders.geil@gmail.com>
2018-04-26 14:52:11 +0200

    Rescaled regression targets
    Signed-off-by: Anders Geil <anders.geil@gmail.com>

--- main.py
+++ main.py
@@ -73,7 +73,8 @@ if classification:
     metrics = ['acc', 'mae', 'mse']
 else:
     # Rescale to [0;1]; merge 0+1
-    label_converter = {0: 0.0, 1: 0.0, 2: 0.03, 3: 0.155, 4: 0.38, 5: 0.63, 6: 0.88}
+    # label_converter = {0: 0.0, 1: 0.0, 2: 0.03, 3: 0.155, 4: 0.38, 5: 0.63, 6: 0.88}
+    label_converter = {0: 0.0, 1: 0.0, 2: 1.0, 3: 2.0, 4: 3.0, 5: 4.0, 6: 5.0}
     loss = "mae"
     metrics = ['acc', 'mae', 'msle']
 labels = {key: label_converter[val] for key, val in labels.items()}
