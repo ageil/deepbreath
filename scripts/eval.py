035011e3980dbebb3aeb6ecd6a9a60a77e924cae
Anders Geil <anders.geil@gmail.com>
2018-05-24 19:49:30 +0200

    Improved output formatting
    Signed-off-by: Anders Geil <anders.geil@gmail.com>

--- scripts/eval.py
+++ scripts/eval.py
@@ -65,32 +65,37 @@ hit = np.sum(preds_int[:,0] == preds_int[:,1])
 miss = np.sum(preds_int[:,0] != preds_int[:,1])
 acc = hit / gen.__len__()
 
-binary0 = df[["Actual", "Predicted"]].clip(0, 1, axis=0)
+binary = df[["Actual", "Predicted"]].clip(0, 1, axis=0)
+binary_hit = np.sum(binary["Actual"] == binary["Predicted"])
+binary_miss = np.sum(binary["Actual"] != binary["Predicted"])
+binary_acc = binary_hit / binary.shape[0]
+
+binary0 = df[["Actual", "Predicted"]].replace([1,2,3,4], 1)
+binary0 = binary0.loc[binary0['Actual'] == 0]
 binary0_hit = np.sum(binary0["Actual"] == binary0["Predicted"])
 binary0_miss = np.sum(binary0["Actual"] != binary0["Predicted"])
 binary0_acc = binary0_hit / binary0.shape[0]
 
 binary1 = df[["Actual", "Predicted"]].replace([0, 2, 3, 4], 0)
-binary1_hit = np.sum(binary1["Actual"] == binary1["Predicted"])
-binary1_miss = np.sum(binary1["Actual"] != binary1["Predicted"])
-binary1_acc = binary1_hit / binary1.shape[0]
-
-binary1 = df[["Actual", "Predicted"]].replace([0, 2, 3, 4], 0)
+binary1 = binary1.loc[binary1['Actual'] == 1]
 binary1_hit = np.sum(binary1["Actual"] == binary1["Predicted"])
 binary1_miss = np.sum(binary1["Actual"] != binary1["Predicted"])
 binary1_acc = binary1_hit / binary1.shape[0]
 
 binary2 = df[["Actual", "Predicted"]].replace([0, 1, 3, 4], 0)
+binary2 = binary2.loc[binary2['Actual'] == 2]
 binary2_hit = np.sum(binary2["Actual"] == binary2["Predicted"])
 binary2_miss = np.sum(binary2["Actual"] != binary2["Predicted"])
 binary2_acc = binary2_hit / binary2.shape[0]
 
 binary3 = df[["Actual", "Predicted"]].replace([0, 1, 2, 4], 0)
+binary3 = binary3.loc[binary3['Actual'] == 3]
 binary3_hit = np.sum(binary3["Actual"] == binary3["Predicted"])
 binary3_miss = np.sum(binary3["Actual"] != binary3["Predicted"])
 binary3_acc = binary3_hit / binary3.shape[0]
 
 binary4 = df[["Actual", "Predicted"]].replace([0, 1, 2, 3], 0)
+binary4 = binary4.loc[binary4['Actual'] == 4]
 binary4_hit = np.sum(binary4["Actual"] == binary4["Predicted"])
 binary4_miss = np.sum(binary4["Actual"] != binary4["Predicted"])
 binary4_acc = binary4_hit / binary4.shape[0]
@@ -106,29 +111,33 @@ df.to_csv(savepath, index=False)
 
 with open(directory + "/" + modelname + ".txt", "w") as txt:
     txt.write("{0}\n\n".format(modelname))
-    txt.write("hit  = {0}\n".format(hit))
-    txt.write("miss = {0}\n".format(miss))
-    txt.write("acc  = {0}\n\n".format(acc))
+    txt.write("hit  = {0}\n".format(round(hit, 2)))
+    txt.write("miss = {0}\n".format(round(miss,2)))
+    txt.write("acc  = {0}\n\n".format(round(acc,2)))
     txt.write("0 vs rest")
-    txt.write("hit  = {0}\n".format(binary0_hit))
-    txt.write("miss = {0}\n".format(binary0_miss))
-    txt.write("acc  = {0}\n\n".format(binary0_acc))
-    txt.write("1 vs rest")
-    txt.write("hit  = {0}\n".format(binary1_hit))
-    txt.write("miss = {0}\n".format(binary1_miss))
-    txt.write("acc  = {0}\n\n".format(binary1_acc))
-    txt.write("2 vs rest")
-    txt.write("hit  = {0}\n".format(binary2_hit))
-    txt.write("miss = {0}\n".format(binary2_miss))
-    txt.write("acc  = {0}\n\n".format(binary2_acc))
-    txt.write("3 vs rest")
-    txt.write("hit  = {0}\n".format(binary3_hit))
-    txt.write("miss = {0}\n".format(binary3_miss))
-    txt.write("acc  = {0}\n\n".format(binary3_acc))
-    txt.write("4 vs rest")
-    txt.write("hit  = {0}\n".format(binary4_hit))
-    txt.write("miss = {0}\n".format(binary4_miss))
-    txt.write("acc  = {0}\n\n".format(binary4_acc))
+    txt.write("hit  = {0}\n".format(round(binary_hit,2)))
+    txt.write("miss = {0}\n".format(round(binary_miss,2)))
+    txt.write("acc  = {0}\n\n".format(round(binary_acc,2)))
+    txt.write("0 only\n")
+    txt.write("hit  = {0}\n".format(round(binary0_hit,2)))
+    txt.write("miss = {0}\n".format(round(binary0_miss,2)))
+    txt.write("acc  = {0}\n\n".format(round(binary0_acc,2)))
+    txt.write("1 only\n")
+    txt.write("hit  = {0}\n".format(round(binary1_hit,2)))
+    txt.write("miss = {0}\n".format(round(binary1_miss,2)))
+    txt.write("acc  = {0}\n\n".format(round(binary1_acc,2)))
+    txt.write("2 only\n")
+    txt.write("hit  = {0}\n".format(round(binary2_hit,2)))
+    txt.write("miss = {0}\n".format(round(binary2_miss,2)))
+    txt.write("acc  = {0}\n\n".format(round(binary2_acc,2)))
+    txt.write("3 only\n")
+    txt.write("hit  = {0}\n".format(round(binary3_hit,2)))
+    txt.write("miss = {0}\n".format(round(binary3_miss,2)))
+    txt.write("acc  = {0}\n\n".format(round(binary3_acc,2)))
+    txt.write("4 only\n")
+    txt.write("hit  = {0}\n".format(round(binary4_hit,2)))
+    txt.write("miss = {0}\n".format(round(binary4_miss,2)))
+    txt.write("acc  = {0}\n\n".format(round(binary4_acc,2)))
 
 print(name)
 print("Overall")
@@ -136,7 +145,7 @@ print("Correct preds:", hit)
 print("Incorrect preds:", miss)
 print("Accuracy:", acc)
 print()
-print("0 vs rest")
-print("Correct preds:", binary0_hit)
-print("Incorrect preds:", binary0_miss)
-print("Accuracy:", binary0_acc)
+print("0 only")
+print("Correct preds:", binary_hit)
+print("Incorrect preds:", binary_miss)
+print("Accuracy:", binary_acc)
